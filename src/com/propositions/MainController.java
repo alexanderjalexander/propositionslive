89c60b4301ff26bd9284cd5d346fa94014b919a7
alexanderjalexander <alexj18082002@gmail.com>
2023-04-17 23:08:49 -0400

    Implemented Complex Evaluation GUI

--- src/com/propositions/MainController.java
+++ src/com/propositions/MainController.java
@@ -6,6 +6,7 @@ import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.geometry.Pos;
+import javafx.scene.Node;
 import javafx.scene.control.*;
 import javafx.scene.control.Alert.*;
 import javafx.scene.layout.HBox;
@@ -25,29 +26,34 @@ public class MainController {
     public void new_simple(ActionEvent e) {
         if (propField.getText().isEmpty()) {
             Alert alert = new Alert(AlertType.ERROR);
-            alert.setTitle("Error");
+            alert.setTitle("Empty Input Error");
             alert.setContentText("Cannot parse an empty string. Try again!");
             alert.show();
         } else {
-
             // Attempt to parse the user input.
+            PropositionInterpreter interp;
+            try {
+                interp = new PropositionInterpreter(propField.getText(), true);
+            } catch (Exception error) {
+                Alert alert = new Alert(AlertType.ERROR);
+                alert.setTitle("Interpreting Error");
+                alert.setContentText("Error when interpreting string '" + propField.getText() + "': "
+                    + error.toString());
+                alert.show();
+                return;
+            }
 
-
+            // Create the new HBox to put inside the ListView
             HBox prop = new HBox(25);
-
+            Button remove = new Button("X");
             Label proposition = new Label(propField.getText());
-            Label result = new Label();
+            Label result = new Label(interp.truth_value ? "TRUE" : "FALSE");
             HBox.setHgrow(result, Priority.ALWAYS);
 
-            Button remove = new Button("X");
             remove.setOnAction(new EventHandler<>() {
-
                 @Override
-                public void handle(ActionEvent e) {
-                    props.remove(prop);
-                }
+                public void handle(ActionEvent e) { props.remove(prop); }
             });
-
 
             prop.setAlignment(Pos.CENTER_LEFT);
             prop.getChildren().addAll(remove, proposition, new Label("->"), result);
@@ -60,19 +66,108 @@ public class MainController {
     }
 
     public void new_complex(ActionEvent e) {
-        /*if (propField.getText().isEmpty()) {
+        if (propField.getText().isEmpty()) {
             Alert alert = new Alert(AlertType.ERROR);
-            alert.setTitle("Error");
+            alert.setTitle("Empty Input Error");
             alert.setContentText("Cannot parse an empty string. Try again!");
             alert.show();
-        } else {
-            names.add(new Button());
-            propView.setItems(names);
         }
-        propField.clear(); */
+        else {
+            // Attempt to parse the user input.
+            PropositionInterpreter interp;
+            try {
+                interp = new PropositionInterpreter(propField.getText(), false, false);
+            } catch (Exception error) {
+                Alert alert = new Alert(AlertType.ERROR);
+                alert.setTitle("Interpreting Error");
+                alert.setContentText("Error when interpreting string '" + propField.getText() + "': "
+                        + error.toString());
+                alert.show();
+                return;
+            }
+
+            // Create the new HBox to put inside the ListView
+            HBox prop = new HBox(25);
+            Button remove = new Button("X");
+            remove.setOnAction(new EventHandler<>() {
+                @Override
+                public void handle(ActionEvent e) { props.remove(prop); }
+            });
+            Label proposition = new Label(propField.getText());
+
+            // Create new text fields for each proposition
+            HBox propvalues = new HBox(10);
+            for (String i : interp.truthmaps.keySet()) {
+                Label proplabel = new Label(i + ": ");
+                TextField input = new TextField();
+                input.setPromptText("\"" + i + "\"'s Truth Value");
+                input.setId(i);
+                propvalues.getChildren().add(proplabel);
+                propvalues.getChildren().add(input);
+            }
+            propvalues.setAlignment(Pos.CENTER_LEFT);
+
+            Label result = new Label(interp.truth_value ? "TRUE" : "FALSE");
+            HBox.setHgrow(result, Priority.ALWAYS);
+            prop.setAlignment(Pos.CENTER_LEFT);
+
+            Button interpret = new Button("INTERPRET");
+            interpret.setOnAction(new EventHandler<>() {
+                @Override
+                public void handle(ActionEvent e) {
+                    // Iterate through, updating the hashmap as needed
+                    for (String i : interp.truthmaps.keySet()) {
+                        for (Node j : propvalues.getChildren()) {
+                            if (j instanceof TextField) {
+                                if ( ((TextField)j).getText().compareToIgnoreCase("t") == 0 ) {
+                                    interp.truthmaps.replace(i, true);
+                                } else if ( ((TextField)j).getText().compareToIgnoreCase("true") == 0 ) {
+                                    interp.truthmaps.replace(i, true);
+                                } else if ( ((TextField)j).getText().compareToIgnoreCase("f") == 0 ) {
+                                    interp.truthmaps.replace(i, false);
+                                } else if ( ((TextField)j).getText().compareToIgnoreCase("false") == 0 ) {
+                                    interp.truthmaps.replace(i, false);
+                                } else {
+                                    Alert alert = new Alert(AlertType.ERROR);
+                                    alert.setTitle("Interpreting Error");
+                                    alert.setContentText("Error when interpreting '" + ((TextField)j).getText() + "': Not a valid input.");
+                                    alert.show();
+                                    return;
+                                }
+                            }
+                        }
+                    }
+                    // Re-evaluate :)
+                    interp.complex_eval();
+                    result.setText(interp.truth_value ? "TRUE" : "FALSE");
+                }
+            });
+
+            prop.getChildren().addAll(remove, proposition, propvalues, interpret, new Label("->"), result);
+
+            props.add(prop);
+            propView.setItems(props);
+        }
+        propField.clear();
     }
 
+    public void insert_conjunction(ActionEvent e) {
+        propField.setText(propField.getText() + "&");
+    }
 
+    public void insert_disjunction(ActionEvent e) {
+        propField.setText(propField.getText() + "|");
+    }
 
+    public void insert_negation(ActionEvent e) {
+        propField.setText(propField.getText() + "!");
+    }
 
+    public void insert_condition(ActionEvent e) {
+        propField.setText(propField.getText() + "->");
+    }
+
+    public void insert_bicondition(ActionEvent e) {
+        propField.setText(propField.getText() + "<->");
+    }
 }
--- src/com/propositions/PropositionInterpreter.java
+++ src/com/propositions/PropositionInterpreter.java
@@ -8,6 +8,10 @@ public class PropositionInterpreter {
 
     public boolean truth_value;
 
+    public HashMap<String, Boolean> truthmaps;
+
+    public PropositionParser parse;
+
     public static class InterpError extends RuntimeException {
         InterpError(String message) {super(message); }
     }
@@ -19,14 +23,15 @@ public class PropositionInterpreter {
 
     public PropositionInterpreter(String proposition, boolean simple) {
         ArrayList<PropositionTokenizer.Token> tokenized = PropositionTokenizer.PropositionTokenize(proposition);
-        PropositionParser parse = new PropositionParser(tokenized);
+        parse = new PropositionParser(tokenized);
         if (simple) {
             // If Simple Evaluation, Assume All Values Are True
             truth_value = simple_evaluator(parse.parsed);
         } else {
             // If Complex Evaluation, Assume All Values Have Diff Types
             // Create a new HashMap to store truthmaps, initially put everything as False
-            HashMap<String, Boolean> truthmaps = complex_prop_hashmap(parse.parsed);
+            truthmaps = complex_prop_hashmap(parse.parsed);
+
             Scanner reader = new Scanner(System.in);
             for (String i : truthmaps.keySet()) {
                 System.out.print("Please input the desired truth-value(\"true\", \"t\", \"false\", \"f\") for the proposition " + i + ": ");
@@ -43,6 +48,43 @@ public class PropositionInterpreter {
                     }
                 }
             }
+
+            // Evaluate using the newly made truthmaps
+            truth_value = complex_evaluator(parse.parsed, truthmaps);
+        }
+    }
+
+    public PropositionInterpreter(String proposition, boolean simple, boolean input) {
+        ArrayList<PropositionTokenizer.Token> tokenized = PropositionTokenizer.PropositionTokenize(proposition);
+        parse = new PropositionParser(tokenized);
+        if (simple) {
+            // If Simple Evaluation, Assume All Values Are True
+            truth_value = simple_evaluator(parse.parsed);
+        } else {
+            // If Complex Evaluation, Assume All Values Have Diff Types
+            // Create a new HashMap to store truthmaps, initially put everything as False
+            truthmaps = complex_prop_hashmap(parse.parsed);
+
+            // If input == true, ask user thru stdin to enter the respective truth values
+            if (input) {
+                Scanner reader = new Scanner(System.in);
+                for (String i : truthmaps.keySet()) {
+                    System.out.print("Please input the desired truth-value(\"true\", \"t\", \"false\", \"f\") for the proposition " + i + ": ");
+                    String tv = reader.nextLine();
+                    while (true) {
+                        if ((tv.compareToIgnoreCase("t") == 0) || (tv.compareToIgnoreCase("true") == 0)) {
+                            truthmaps.replace(i, true);
+                            break;
+                        } else if ((tv.compareToIgnoreCase("f") == 0) || (tv.compareToIgnoreCase("false") == 0)) {
+                            truthmaps.replace(i, false);
+                            break;
+                        } else {
+                            System.out.println("\"" + tv + "\" is not a valid input. Please try again.");
+                        }
+                    }
+                }
+            }
+
             // Evaluate using the newly made truthmaps
             truth_value = complex_evaluator(parse.parsed, truthmaps);
         }
@@ -58,7 +100,7 @@ public class PropositionInterpreter {
         } else {
             // If Complex Evaluation, Assume All Values Have Diff Types
             // Create a new HashMap to store truthmaps, initially put everything as False
-            HashMap<String, Boolean> truthmaps = complex_prop_hashmap(parse.parsed);
+            truthmaps = complex_prop_hashmap(parse.parsed);
             Scanner reader = new Scanner(in);
             for (String i : truthmaps.keySet()) {
                 out.print("Please input the desired truth-value(\"true\", \"t\", \"false\", \"f\") for the proposition " + i + ": ");
@@ -125,76 +167,80 @@ public class PropositionInterpreter {
     // Recursive Method that returns a hashmap containing all of the propositions with the
     public static HashMap<String, Boolean> complex_prop_hashmap(Expr root) {
         // Initialize Hashmap [truthmaps]
-        HashMap<String, Boolean> truthmaps = new HashMap<>();
+        HashMap<String, Boolean> truthmap = new HashMap<>();
 
         // Call on recursive helper method to update truthmaps
         // Initialize all values to false
-        complex_prop_hashmap_helper(root, truthmaps);
+        complex_prop_hashmap_helper(root, truthmap);
 
-        // Return [truthmaps]
-        return truthmaps;
+        // Return [truthmap]
+        return truthmap;
     }
 
 
-    private static void complex_prop_hashmap_helper(Expr root, HashMap<String, Boolean> truthmaps) {
+    private static void complex_prop_hashmap_helper(Expr root, HashMap<String, Boolean> truthm) {
         // Traverse the tree left and right,
         if (root instanceof Expr.BinaryOperation) {
             // Go left and right
-            complex_prop_hashmap_helper( ((Expr.BinaryOperation)root).left, truthmaps );
-            complex_prop_hashmap_helper( ((Expr.BinaryOperation)root).right, truthmaps );
+            complex_prop_hashmap_helper( ((Expr.BinaryOperation)root).left, truthm );
+            complex_prop_hashmap_helper( ((Expr.BinaryOperation)root).right, truthm );
         } else if (root instanceof Expr.Unary) {
             // UNARY Operations, go into expression
-            complex_prop_hashmap_helper( ((Expr.Unary)root).expression, truthmaps );
+            complex_prop_hashmap_helper( ((Expr.Unary)root).expression, truthm );
         } else if (root instanceof Expr.Grouping) {
             // Groupings
-            complex_prop_hashmap_helper( ((Expr.Grouping)root).expression, truthmaps );
+            complex_prop_hashmap_helper( ((Expr.Grouping)root).expression, truthm );
         } else {
             // Literals, just use truth values of the actual node
-            truthmaps.putIfAbsent(((Expr.Literal) root).name.toString(), false);
+            truthm.putIfAbsent(((Expr.Literal) root).name.toString(), false);
         }
+    }
+
+    public void complex_eval() {
+        truth_value = complex_evaluator(parse.parsed, truthmaps);
     }
 
     // Recursive In-Order Proposition Searcher
     // Searches for all given proposition with truthmaps to determine whether a boolean has a truth or not
-    public static boolean complex_evaluator(Expr root, HashMap<String, Boolean> truthmaps) {
+    public static boolean complex_evaluator(Expr root, HashMap<String, Boolean> truthm) {
         // Evaluate according to an in-order traversal
         // Evaluates based on NAME tokens' truth values
         if (root instanceof Expr.BinaryOperation) {
             // Either an AND, OR, IMPLIES, or IFF
             switch(((Expr.BinaryOperation)root).operation.type) {
                 case AND -> {
-                    boolean left = complex_evaluator(((Expr.BinaryOperation)root).left, truthmaps );
-                    boolean right = complex_evaluator(((Expr.BinaryOperation)root).right, truthmaps );
+                    boolean left = complex_evaluator(((Expr.BinaryOperation)root).left, truthm );
+                    boolean right = complex_evaluator(((Expr.BinaryOperation)root).right, truthm );
                     return Propositions.and(left, right);
                 }
                 case OR -> {
-                    boolean left = complex_evaluator(((Expr.BinaryOperation)root).left, truthmaps );
-                    boolean right = complex_evaluator(((Expr.BinaryOperation)root).right, truthmaps );
+                    boolean left = complex_evaluator(((Expr.BinaryOperation)root).left, truthm );
+                    boolean right = complex_evaluator(((Expr.BinaryOperation)root).right, truthm );
                     return Propositions.or(left, right);
                 }
                 case IMPLIES -> {
-                    boolean left = complex_evaluator(((Expr.BinaryOperation)root).left, truthmaps );
-                    boolean right = complex_evaluator(((Expr.BinaryOperation)root).right, truthmaps );
+                    boolean left = complex_evaluator(((Expr.BinaryOperation)root).left, truthm );
+                    boolean right = complex_evaluator(((Expr.BinaryOperation)root).right, truthm );
                     return Propositions.implies(left, right);
                 }
                 case IF_AND_ONLY_IF -> {
-                    boolean left = complex_evaluator(((Expr.BinaryOperation)root).left, truthmaps );
-                    boolean right = complex_evaluator(((Expr.BinaryOperation)root).right, truthmaps );
+                    boolean left = complex_evaluator(((Expr.BinaryOperation)root).left, truthm );
+                    boolean right = complex_evaluator(((Expr.BinaryOperation)root).right, truthm );
                     return Propositions.ifAndOnlyIf(left, right);
                 }
                 default -> throw error(((Expr.BinaryOperation)root).operation, "Binary Operation Expression has invalid Operation Token");
             }
         } else if (root instanceof Expr.Unary) {
             // UNARY Operations
-            return Propositions.not( complex_evaluator( ((Expr.Unary)root).expression, truthmaps ) );
+            return Propositions.not( complex_evaluator( ((Expr.Unary)root).expression, truthm ) );
         } else if (root instanceof Expr.Grouping) {
             // Groupings
-            return complex_evaluator( ((Expr.Grouping)root).expression, truthmaps );
+            return complex_evaluator( ((Expr.Grouping)root).expression, truthm );
         } else {
             // Literals, just use truth values of the actual node
             //return ((Literal) root).name.truth_value;
-            if (truthmaps.get( ((Expr.Literal) root).name.toString() ) != null) {
-                return truthmaps.get( ((Expr.Literal) root).name.toString() );
+            if (truthm.get( ((Expr.Literal) root).name.toString() ) != null) {
+                return truthm.get( ((Expr.Literal) root).name.toString() );
             } else {
                 throw error(((Expr.Literal) root).name, "No Truth-Value provided for the proposition.");
             }
