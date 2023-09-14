2e8bd76f408a4a9a739d6818741b5a7d4454d43c
alexanderjalexander <alexj18082002@gmail.com>
2023-09-14 18:24:18 -0400

    Truth Table 90% implemented, need to fix bugs

--- src/com/app/Main.java
+++ src/com/app/Main.java
@@ -3,9 +3,13 @@ package com.app;
 import com.propositions.PropositionInterpreter;
 import com.propositions.PropositionParser;
 import javafx.application.*;
+import javafx.beans.property.ReadOnlyStringWrapper;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.control.MenuItem;
+import javafx.scene.control.cell.MapValueFactory;
 import javafx.stage.*;
 import javafx.scene.*;
 import javafx.fxml.FXMLLoader;
@@ -16,7 +20,9 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import java.io.StringWriter;
 import java.net.URI;
+import java.util.ArrayList;
 import java.util.Objects;
+import java.util.List;
 
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
@@ -139,7 +145,7 @@ public class Main extends Application {
     private MenuItem darkModeMenu;
 
     @FXML
-    private TableView truth_table = new TableView();
+    private TableView<List<String>> truth_table = new TableView<>();
 
 
     public void userAlert(String title, String message, Exception error) {
@@ -439,13 +445,93 @@ public class Main extends Application {
                     error);
             return;
         }
+
+        // Create two arrays, one with the names, one with the truth values
+        ArrayList<String> names = new ArrayList<>();
+        ArrayList<Boolean> values = new ArrayList<>();
+        ArrayList<ArrayList<Boolean>> results = new ArrayList<>();
+
+        // The results string will hold the columns of the truth values
+        // Thus, it will look something like this
+        // {
+        //  "P"      [T,T,F,F]
+        //  "Q"      [T,F,T,F]
+        //  "P & Q"  [T,F,F,F]
+        // }
+
         // For every proposition within the hashmap provided, create new columns in the table
         for (String i : interp.truthmaps.keySet()) {
-            truth_table.getColumns().add(new TableColumn<>(i));
-        }
-        truth_table.getColumns().add(new TableColumn<>(truthField.getText()));
+            // Also set it to true
+            interp.truthmaps.replace(i, true);
 
-        //
+            // Add the names to the values
+            names.add(i);
+            values.add(interp.truthmaps.get(i));
+
+            // Initiate results with "columns" representing each prop
+            results.add(new ArrayList<Boolean>());
+        }
+
+        // Add another row for the result column's rows
+        results.add(new ArrayList<Boolean>());
+
+        // Calculate every possible value using the new values
+        do {
+            for (int i = 0; i < names.size(); i++) {
+                interp.truthmaps.replace(names.get(i), values.get(i));
+                results.get(i).add(values.get(i));
+            }
+            interp.complex_eval();
+            results.get(results.size() - 1).add(interp.truth_value);
+        } while(truth_next_assignment(values));
+
+        // Name Column Creation
+        for (int i = 0; i < names.size(); i++) {
+            // Create the new column
+            TableColumn<List<String>, String> col = new TableColumn<>(names.get(i));
+            col.setSortable(false);
+            final int temp = i;
+            col.setCellValueFactory(data -> {
+                return new ReadOnlyStringWrapper(data.getValue().get(temp));
+            });
+            truth_table.getColumns().add(col);
+        }
+
+        // Create a final column for the proposition as a whole
+        TableColumn<List<String>, String> col = new TableColumn<>(truthField.getText());
+        col.setSortable(false);
+        col.setCellValueFactory(data -> {
+            return new ReadOnlyStringWrapper(data.getValue().get(2));
+        });
+        truth_table.getColumns().add(col);
+
+        // Add data to rows one by one
+        for (int i = 0; i < results.get(0).size(); i++) {
+            ArrayList<String> row = new ArrayList<>();
+            for (int j = 0; j < truth_table.getColumns().size(); j++) {
+                row.add(results.get(j).get(i) ? "T" : "F");
+            }
+            truth_table.getItems().add(row);
+        }
+
+    }
+
+    private boolean truth_next_assignment(ArrayList<Boolean> values) {
+        // Walking from Right to Left, search for a True to make False
+        int flipIndex = values.size() - 1;
+        while ((flipIndex >= 0) && !values.get(flipIndex)) { flipIndex--; }
+
+        // If none found, return false and finish, none found to flip
+        if (flipIndex == -1) return false;
+
+        /* Otherwise, flip this index to true and all following values to false. */
+        values.set(flipIndex, false);
+
+        for (int i = flipIndex + 1; i < values.size(); i++) {
+            values.set(i, true);
+        }
+
+        return true;
     }
 
     public void insert_conjunction() {
